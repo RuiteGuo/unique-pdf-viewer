04ae0601f2a2fbeca143ad2b313b074a11dd8fbc
lizekang <zekangli@hustunique.com>
2017-05-14 12:49:00 +0800

    html

--- js/viewer.js
+++ js/viewer.js
@@ -1,3 +1,6 @@
+/**
+ * Created by lzk on 5/14/17.
+ */
 
 var winHeight;
 if (window.innerHeight)
@@ -14,69 +17,70 @@ var config = {
 var tempOffset = 0, offset;
 let pageArray = [];
 let canvasArray = [];
+let imageArray = [];
 
 PDFJS.workerSrc = 'lib/pdf.worker.js';
 
 
 function ViewerDomCreator(viewer, numberofpages){                                // 创建viewer DOM元素里的DOM结构
-                                                                                 // viewer为DOM元素
-  for(var i = 1; i <= numberofpages; i ++) {       
-      var page = document.createElement('div');
-      viewer.appendChild(page);
-      page.className = "page";
-      
-      var canvasWrapper = document.createElement('div');
-      page.appendChild(canvasWrapper);
-      canvasWrapper.className = "canvasWrapper";
-      
-      var textLayer = document.createElement('div');
-      page.appendChild(textLayer);
-      textLayer.className = "textLayer";
+    // viewer为DOM元素
+    for(var i = 1; i <= numberofpages; i ++) {
+        var page = document.createElement('div');
+        viewer.appendChild(page);
+        page.className = "page";
 
-      var canvas = document.createElement('canvas');
-      canvasWrapper.appendChild(canvas);
-      canvas.id = "page" + i;
-  }
+        var canvasWrapper = document.createElement('div');
+        page.appendChild(canvasWrapper);
+        canvasWrapper.className = "canvasWrapper";
+
+        var textLayer = document.createElement('div');
+        page.appendChild(textLayer);
+        textLayer.className = "textLayer";
+
+        var canvas = document.createElement('canvas');
+        canvasWrapper.appendChild(canvas);
+        canvas.id = "page" + i;
+    }
 }
 
 
 
 function pageTextRender(page, canvas) {                          // 文本渲染成div元素，page为PDFPageProxy对象
-    
-    var textContent = null; 
+
+    var textContent = null;
     page.getTextContent().then(function(context){               // 获得文本信息，context为TextContent对象
-            textContent = context;
-            console.log(textContent);
-            for (var item of textContent.items) {
-                var canvasWrapper = canvas.parentNode;
-                canvasWrapper.style.height = canvas.style.height;
-                canvasWrapper.style.width = canvas.style.width;
+        textContent = context;
+        console.log(textContent);
+        for (var item of textContent.items) {
+            var canvasWrapper = canvas.parentNode;
+            canvasWrapper.style.height = canvas.style.height;
+            canvasWrapper.style.width = canvas.style.width;
 
-                var page = canvasWrapper.parentNode;
-                page.style.height = canvas.style.height;
-                page.style.width = canvas.style.width;
+            var page = canvasWrapper.parentNode;
+            page.style.height = canvas.style.height;
+            page.style.width = canvas.style.width;
 
-                var textLayer = canvasWrapper.nextSibling;
-                textLayer.style.height = canvas.style.height;
-                textLayer.style.width = canvas.style.width;
+            var textLayer = canvasWrapper.nextSibling;
+            textLayer.style.height = canvas.style.height;
+            textLayer.style.width = canvas.style.width;
 
-                var textItem = document.createElement('div');
-                textLayer.appendChild(textItem);
+            var textItem = document.createElement('div');
+            textLayer.appendChild(textItem);
 
-                textItem.innerHTML = item.str;
-                textItem.style.fontSize = item.transform[0] * SCALE + 'px';
-                textItem.style.fontFamily = 'sans-serif';
-                textItem.style.width = item.width * SCALE+ 'px';
-                textItem.style.left = item.transform[4] * SCALE+ 'px';   // item.transform 结构同下面的transform解释
-                textItem.style.top = (canvas.height/SCALE - item.transform[5] - item.transform[0] + 3) * SCALE + 'px';
-            }
+            textItem.innerHTML = item.str;
+            textItem.style.fontSize = item.transform[0] * SCALE + 'px';
+            textItem.style.fontFamily = 'sans-serif';
+            textItem.style.width = item.width * SCALE+ 'px';
+            textItem.style.left = item.transform[4] * SCALE+ 'px';   // item.transform 结构同下面的transform解释
+            textItem.style.top = (canvas.height/SCALE - item.transform[5] - item.transform[0] + 3) * SCALE + 'px';
+        }
     }) ;
 }
 
 
 
 function pageRender(page) {                                 // page为PDFPageProxy对象
-    
+
     var viewport = page.getViewport(SCALE);                      // viewport为PageViewport对象
 
     var canvas = document.getElementById('page' + page.pageNumber);
@@ -92,36 +96,36 @@ function pageRender(page) {                                 // page为PDFPagePro
     };
     renderContext.push (tempRenderContext
 //      transform: [1,0,0,1,100,1000]                           // transform 第一个参数是水平长度比例，第四个是竖直长度比例，第二三个为旋转
-                                                                // 第五个参数代表水平位置（对于左边框），第六个参数代表竖直位置（对于上边框），
-                                                                // 返回的item里的竖直位置好像是对于下边框
+        // 第五个参数代表水平位置（对于左边框），第六个参数代表竖直位置（对于上边框），
+        // 返回的item里的竖直位置好像是对于下边框
     );
     //console.log(tempRenderContext);
     var renderTask = page.render(tempRenderContext);
     renderTask.then(function () {                               // renderTask属于 RenderTask 类
-      console.log('Page rendered');
+        console.log('Page rendered');
     });
 }
 
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
 
-    alert("1");
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-  });
+        alert("1");
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+    });
 
 
 
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 
-  for ( var item in response) {
-      if (item === "URL") {
-        PDFURL = response.URL;
-        init();
-      }
-  }
+    for ( var item in response) {
+        if (item === "URL") {
+            PDFURL = response.URL;
+            init();
+        }
+    }
 });
 
 function init() {
@@ -131,7 +135,7 @@ function init() {
 
         var NumberOfPages = pdf.numPages;                              // 获得pdf页数
         var viewer = document.getElementById('viewer');
-    
+
         ViewerDomCreator(viewer, NumberOfPages);
 
 
@@ -140,11 +144,11 @@ function init() {
             pdf.getPage(pageNumber).then(function(page) {
                 pageArray.push(page);
                 pageRender(page);
-            });               
+            });
         }
 
     }, function (reason) {
-            console.error(reason);
+        console.error(reason);
     }).then(function () {
         Object.defineProperty(config, 'scale', {
             set: function (value) {
@@ -156,7 +160,7 @@ function init() {
                         canvasArray[i].width = renderContext[i].viewport.width;
                         renderContext[i].canvasContext = canvasArray[i].getContext('2d');
                         pageArray[i].render(renderContext[i]);
-                    // console.log("*****************");
+                        // console.log("*****************");
                     }
                 }
             },
@@ -166,22 +170,7 @@ function init() {
         })
     });
 }
-//
-// function renderWrapper(i){
-//     let xun = render(i);
-//     xun.promise.then(function () {
-//         i++;
-//         if(i<pageArray.length)
-//             renderWrapper(i);
-//     })
-// }
-// function render(i){
-//     renderContext[i].viewport = pageArray[i].getViewport(config.scale);
-//     canvasArray[i].height = renderContext[i].viewport.height;
-//     canvasArray[i].width = renderContext[i].viewport.width;
-//     renderContext[i].canvasContext = canvasArray[i].getContext('2d');
-//     pageArray[i].render(renderContext[i]);
-// }
+
 
 //get mouse position
 var getCoordInDocument = function(e) {
